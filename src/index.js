25fc2c2f93c9dc4d0facbf31225ddfefb6b0c4d1
gyloff <31848467+gyloff@users.noreply.github.com>
2017-09-15 00:37:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
  var st = [];
  var kof = 0;
- for (var g = 0; g < str.length; g++){
-   if (str[g] == '2') str[g] = '1';
-   if (str[g] == '4') str[g] = '3'; 
-   if (str[g] == '6') str[g] = '5';
- }
- for (var k = 0; k < bracketsConfig.length; k++){
-  if (bracketsConfig[k][1] == '2') bracketsConfig[k][1] = '1';
-  if (bracketsConfig[k][1] == '4') bracketsConfig[k][1] = '3';
-  if (bracketsConfig[k][1] == '6') bracketsConfig[k][1] = '5';
-}
  for (var i = 0; i < str.length; i++){
    for (var j = 0; j < bracketsConfig.length; j++){
     if (bracketsConfig[j][0] == bracketsConfig[j][1] && bracketsConfig[j][0] == str[i]){
