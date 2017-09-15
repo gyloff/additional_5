56059ea3a501e81c0031d007b3921ea664d73f26
gyloff <31848467+gyloff@users.noreply.github.com>
2017-09-15 13:01:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
  var st = [];
- var kof = 0;
  for (var i = 0; i < str.length; i++){
    for (var j = 0; j < bracketsConfig.length; j++){
     if (bracketsConfig[j][0] == bracketsConfig[j][1] && bracketsConfig[j][0] == str[i]){
-       if (bracketsConfig[j][0] != st[st.lenght-1]) {
+      if (st[st.length-1] == 0 || st[st.length-1] != str[i]) {
          st.push(str[i]);
-         kof++;
          break;
-       }else{
-         if (bracketsConfig[j][0] == st[st.lenght-1]){
+       }else {
            st.pop();
-           kof--;
            break;
-         }
        }
      }
      if (str[i] == bracketsConfig[j][0]){
        st.push(str[i]);
-       kof++;
        break;
-     }else {
-       if (str[i] == bracketsConfig[j][1] && bracketsConfig[j][0] == st[st.length-1]){
-         st.pop();
-         kof--;
-         break;
-       }
-     }
+     }else 
+        if (str[i] == bracketsConfig[j][1]){
+          if(st.length != 0){
+            if (bracketsConfig[j][0] == st[st.length-1]){
+              st.pop();
+              break;
+            }  else return false;    
+          }  else return false;
+        }
+      }
     }
- }
- if (kof == 0) return true; else return false;
+ return (st.length == 0);
 }
